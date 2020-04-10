82afcdf87474b617305d53e6c0b1a973d860ec5f
Fred-Reis <fredgost@hotmail.com>
2020-04-10 03:35:58 -0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,38 @@
 const express = require('express');
-const {uuid} = require('uuidv4')
+const {uuid, isUuid} = require('uuidv4')
 
 const app = express();
 
 // esse comando é para receber o json no body
 app.use(express.json())
+
+function logRequest(request, response, next) {
+  
+  const {method, url} = request;
+
+  const logLabel = `[${method.toUpperCase()}] ${url}`
+  console.time(logLabel)
+
+  next()
+
+  console.timeEnd(logLabel)
+
+}
+
+function validateId(request, response, next){
+
+  const {id} = request.params
+
+  if (!isUuid(id)){
+
+    return response.status(400).json({error: 'ID not exists!'})
+  }
+
+  return next()
+}
+
+app.use(logRequest)
+app.use('/projects/:id', validateId)
 
 const projects = []
 
